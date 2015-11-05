6ccb297be6b0705ac7cee25536b8980a5860d186
Markus Sandy <markus.sandy@gmail.com>
2015-11-04 23:06:44 -0500

    specify users columns

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
-select * from users;
+select uid, name, mail, pass, status, picture, created, access, login, language, timezone, data, init, signature, signature_format, theme  from users;
 
 select fid, uid, filename, uri, filemime, filesize, status, tiestamp from file_managed;
 
